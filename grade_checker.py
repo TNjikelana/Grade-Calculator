9d3bf1ed0e1d6610501077b2a52a7ba911c85414
TNjikelana <tnjikela021@student.wethinkcode.co.za>
2025-09-26 01:47:15 +0200

    Wecome: print statemts

---
+++ grade_checker.py
@@ -0,0 +1,26 @@
+def welcome():
+    message = print("\n||========================================================================================================||\n||\t\t\t\t\t\t\t\t\t\t\t\t\t  ||\n" \
+    "||   Hi, welcome to your interactive grade calculator(and predictor?)\t\t\t\t\t  ||"
+    "\n||   This cmd calculator will help you determine your current year mark and what you need to pass.\t  ||\n||\t\t\t\t\t\t\t\t\t\t\t\t\t  ||\n" \
+    "||   You can check you mark progress for the follwoing modules:\t\t\t\t\t\t  ||\n||\t\t\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [1] ADF 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [2] PRG 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [3] CNF 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [4] ICF 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [5] MUF 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [6] PRC 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [7] BRP 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||  [8] PRT 152S\t\t\t\t\t\t\t\t\t\t\t  ||\n||\t\t\t\t\t\t\t\t\t\t\t\t\t  ||\n"\
+        "||========================================================================================================||\n")
+
+
+    return message
+
+
+
+
+def main():
+    welcome()
+
+if __name__ == "__main__":
+    main()
